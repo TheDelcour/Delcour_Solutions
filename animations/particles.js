49e66a771ffdd712ce357688cb140200bc5dd108
Tobias Delcour <tobiasdelcour@hotmail.com>
2020-09-17 21:26:56 +0200

    Changed particle animation | fixed some positioning | created page nav and started building page content
    Particle animation made more soothering with the use of circles, created blink function and made them follow a path.
    
    Fixed the positions to be relative and made the page scrollable.
    
    Started working on the next part of the  page with a in-page menu (menu still needs styling)

--- animations/particles.js
+++ animations/particles.js
@@ -3,38 +3,77 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var context = canvas.getContext("2d");
-var particles = [];
+var    max_particles = 120;
+var   draw_interval = 40;
+pixies = new Array();
+gradient = null;
 
-function random (min, max) {
-  return Math.random() * (max - min) + min;
+function Circle() {
+    this.settings = {ttl:8000, xmax:5, ymax:2, rmax:10, rt:1, xdef:960, ydef:540, xdrift:4, ydrift: 4, random:true, blink:true};
+
+    this.reset = function() {
+        this.x = (this.settings.random ? canvas.width*Math.random() : this.settings.xdef);
+        this.y = (this.settings.random ? canvas.height*Math.random() : this.settings.ydef);
+        this.r = ((this.settings.rmax-1)*Math.random()) + 1;
+        this.dx = (Math.random()*this.settings.xmax) * (Math.random() < .5 ? -1 : 1);
+        this.dy = (Math.random()*this.settings.ymax) * (Math.random() < .5 ? -1 : 1);
+        this.hl = (this.settings.ttl/draw_interval)*(this.r/this.settings.rmax);
+        this.rt = Math.random()*this.hl;
+        this.settings.rt = Math.random()+1;
+        this.stop = Math.random()*.2+.4;
+        this.settings.xdrift *= Math.random() * (Math.random() < .5 ? -1 : 1);
+        this.settings.ydrift *= Math.random() * (Math.random() < .5 ? -1 : 1);
+    }
+
+    this.fade = function() {
+        this.rt += this.settings.rt;
+    }
+
+    this.draw = function() {
+        if(this.settings.blink && (this.rt <= 0 || this.rt >= this.hl)) {
+            this.settings.rt = this.settings.rt*-1;
+        } else if(this.rt >= this.hl) {
+            this.reset();
+        }
+
+        var newo = 1-(this.rt/this.hl);
+        context.beginPath();
+        context.arc(this.x, this.y, this.r, 0, Math.PI*2, true);
+        context.closePath();
+
+        var cr = this.r*newo;
+        gradient = context.createRadialGradient(this.x, this.y, 0, this.x, this.y, (cr <= 0 ? 1 : cr));
+        gradient.addColorStop(0.0, 'rgba(255,255,255,'+newo+')');
+        gradient.addColorStop(this.stop, 'rgba(77,101,181,'+(newo*.6)+')');
+        gradient.addColorStop(1.0, 'rgba(77,101,181,0)');
+        context.fillStyle = gradient;
+        context.fill();
+    }
+
+    this.move = function() {
+        this.x += (this.rt/this.hl)*this.dx;
+        this.y += (this.rt/this.hl)*this.dy;
+        if(this.x > canvas.width || this.x < 0) this.dx *= -1;
+        if(this.y > canvas.height || this.y < 0) this.dy *= -1;
+    }
+
+    this.getX = function() { return this.x; }
+    this.getY = function() { return this.y; }
+}
+
+
+for (var i = 0; i < max_particles; i++) {
+    pixies.push(new Circle());
+    pixies[i].reset();
 }
 
 function draw() {
-  const particle = {
-    x: random(0,canvas.width),
-    y: random(0,canvas.height),
-    xvel: Math.random() * 0.8,
-    yvel: Math.random() * 0.8,
-    color: `rgba(${random(250, 255)}, ${random(100, 153)}, ${random(0, 0)})`,
-    size: 7,
-  };
-
-  particles.push(particle);
-
-  if (particles.length > 200) {
-    particles.shift();
-  }
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (let i = 0; i < particles.length; i += 1){
-    const p = particles[i];
-    context.fillStyle = p.color;
-    context.fillRect(p.x, p.y, p.size, p.size);
-    p.x += p.xvel;
-    p.y -= p.yvel;
-  }
-
-  window.requestAnimationFrame(draw);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for(var i = 0; i < pixies.length; i++) {
+        pixies[i].fade();
+        pixies[i].move();
+        pixies[i].draw();
+    }
 }
 
-window.requestAnimationFrame(draw);
+setInterval(draw, draw_interval);
